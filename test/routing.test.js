609df122b4d7df58071be3c962b53e0ff2020341
jake.lewis <jake.lewis@baesystems.com>
2018-11-12 11:07:16 +0000

    Routing test updates

--- test/routing.test.js
+++ test/routing.test.js
@@ -118,6 +118,73 @@ describe('Test RoutingController', () => {
             });
         });
     });
+    describe('removing a websocket', () => {
+        let userId = 'userId';
+        let httpSessionId = 'httpSessionId';
+        let userIdFunc = jest.fn((ws) => userId);
+        let httpSessionIdFunc = jest.fn((ws) => httpSessionId);
+        let socket = {
+            on: jest.fn(),
+            send: jest.fn(x => x)
+        };
+        test('Invalid socket', () => {
+            let controller = new RoutingController(userIdFunc, httpSessionIdFunc)
+            controller.addWebsocket(socket);
+            expect(() => controller.removeWebsocket({})).toThrowError(/^Socket parameter/);
+        });
+        test('Single websocket', () => {
+            let controller = new RoutingController(userIdFunc, httpSessionIdFunc)
+            controller.addWebsocket(socket);
+            expect(controller.removeWebsocket(socket)).toBeTruthy();
+            expect(controller.userMap.get(userId)).toBeUndefined();
+        });
+        test('Multiple websockets', () => {
+            let controller = new RoutingController(jest.fn((ws) => ws.id), jest.fn((ws) => ws.sessionId))
+            let firstSocket = {
+                id: 'user',
+                sessionId: 1,
+                on: jest.fn(),
+                send: jest.fn(x => x)
+            };
+            let secondSocket = {
+                id: 'user',
+                sessionId: 2,
+                on: jest.fn(),
+                send: jest.fn(x => x)
+            };
+
+            controller.addWebsocket(firstSocket);
+            controller.addWebsocket(secondSocket);
+            expect(controller.removeWebsocket(firstSocket)).toBeTruthy();
+            expect(controller.userMap.get('user')).not.toBeUndefined();
+            expect(controller.userMap.get('user').get(2)).not.toBeUndefined();
+        });
+        test('Undefined session map', () => {
+            let controller = new RoutingController(userIdFunc, httpSessionIdFunc)
+            expect(controller.removeWebsocket(socket)).toBeFalsy();
+        });
+    });
+    describe('check if user has a session', () => {
+        let userId = 'userId';
+        let httpSessionId = 'httpSessionId';
+        let userIdFunc = jest.fn((ws) => userId);
+        let httpSessionIdFunc = jest.fn((ws) => httpSessionId);
+        let controller = new RoutingController(userIdFunc, httpSessionIdFunc)
+        let socket = {
+            on: jest.fn(),
+            send: jest.fn(x => x)
+        };
+        controller.addWebsocket(socket);
+
+        test('user has session', () => {
+            let result = controller.userHasSession(userId, httpSessionId);
+            expect(result).toBeTruthy()
+        });
+        test('user doesnt have session', () => {
+            let result = controller.userHasSession(userId, 'new session id');
+            expect(result).toBeFalsy()
+        })
+    });
     describe('sendingPayload', () => {
         let userId = 'userId';
         let httpSessionId = 'httpSessionId';
@@ -164,6 +231,19 @@ describe('Test RoutingController', () => {
                 expect(socket.send).toReturnTimes(1);
                 expect(socket.send).toHaveReturnedWith('example');
             });
+            test('sending to same socket, no username', () => {
+                let controller = new RoutingController(userIdFunc, httpSessionIdFunc);
+                let socket = {
+                    on: jest.fn(),
+                    send: jest.fn(x => x)
+                };
+                controller.addWebsocket(socket);
+
+                controller.sendToHttpSession('example', httpSessionId);
+
+                expect(socket.send).toReturnTimes(1);
+                expect(socket.send).toHaveReturnedWith('example');
+            });
             test('sending to different socket', () => {
                 let controller = new RoutingController(userIdFunc, httpSessionIdFunc);
                 let socket = {
@@ -178,8 +258,25 @@ describe('Test RoutingController', () => {
             })
         });
         describe('sending to All', () => {
-            test('sending to different socket', () => {
-                // TODO
+            test('two mock sockets', () => {
+                let controller = new RoutingController(userIdFunc, httpSessionIdFunc);
+                let firstSocket = {
+                    on: jest.fn(),
+                    send: jest.fn(x => x)
+                };
+                let secondSocket = {
+                    on: jest.fn(),
+                    send: jest.fn(x => x)
+                };
+                controller.addWebsocket(firstSocket);
+                controller.addWebsocket(secondSocket);
+
+                controller.sendToAll('payload');
+
+                expect(firstSocket.send).toReturnTimes(1);
+                expect(firstSocket.send).toHaveReturnedWith('payload');
+                expect(secondSocket.send).toReturnTimes(1);
+                expect(secondSocket.send).toHaveReturnedWith('payload');
             });
         })
     });
