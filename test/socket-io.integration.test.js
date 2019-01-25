56457f4d96fbf6484959b4a5aac97a4dbd4a9db3
jake.lewis <jake.lewis@baesystems.com>
2019-01-25 14:03:10 +0000

    Test updates

--- test/socket-io.integration.test.js
+++ test/socket-io.integration.test.js
@@ -10,12 +10,35 @@ describe('Socket IO integration test', () => {
 
     app.use(middle);
 
+    app.get('/sendUser', (req, res) => {
+        req.user.id = 'user1234';
+        Clacks.sendUser('user test');
+        res.send('GET Request');
+    });
+
+    app.get('/sendSession', (req, res) => {
+
+    });
+
     app.get('/sendAll', (req, res) => {
-        Clacks.sendAll('test');
+        Clacks.sendAll('send all test');
         res.send('GET Request');
     });
 
     server.listen(8378);
+
+    test('Send User, single client socket', () => {
+        let socket = io_client('localhost:8378');
+        let lastMessage = null;
+        socket.on('message', (message) => {
+            lastMessage = message
+        });
+        http.get('http://localhost:8378/sendUser', (res) => {
+            expect(res.body).toBe('GET Request');
+            expect(lastMessage).toBe('user test');
+        });
+        socket.disconnect();
+    });
 
     test('Send All, single client socket', () => {
         let socket = io_client('localhost:8378');
@@ -25,8 +48,8 @@ describe('Socket IO integration test', () => {
         });
         http.get('http://localhost:8378/sendAll', (res) => {
             expect(res.body).toBe('GET Request');
-            expect(lastMessage).toBe('test');
+            expect(lastMessage).toBe('send all test');
         });
-
+        socket.disconnect();
     });
 });
